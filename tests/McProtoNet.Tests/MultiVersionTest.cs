c736edd41bea96977d355b596376b4717452006b
IcySnex <81762750+IcySnex@users.noreply.github.com>
2025-10-03 15:09:18 +0200

    Revert "a"
    This reverts commit 728d2a46fd18590eb60f56faae6abec2c220971a.

---
+++ tests/McProtoNet.Tests/MultiVersionTest.cs
@@ -0,0 +1,21 @@
+﻿using McProtoNet.Protocol;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace McProtoNet.Tests;
+
+[TestClass]
+public class MultiVersionTest
+{
+    [TestMethod]
+    public void TestMultiVersion()
+    {
+        int version = 477;
+        
+    }
+
+    [TestMethod]
+    public void PacketFactoryInit()
+    {
+        PacketFactory.CreateClientboundPacket(340, 0x00, PacketState.Play);
+    }
+}
